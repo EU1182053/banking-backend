c8b801417bde3c16143524682862a88d35ddae27
siddhesh <siddheshs871@gmail.com>
2022-11-30 15:34:06 +0530

    29-11-22

--- banking/src/main/java/com/banking/repository/BeneficiaryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.banking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.banking.model.Beneficiary;
-
-public interface BeneficiaryRepository extends JpaRepository<Beneficiary, Integer> {
-
-}
